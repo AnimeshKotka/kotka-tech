05087b3fed655906e6477e17b0dd5af4505c1a11
AnimeshKotka-VaraTech <animesh.kotka@infrovate.com>
2024-11-28 18:28:34 +0530

    Deploy website - based on 0c0f15450a11c6eae2b08173cfd2eb4466415b72

---
+++ assets/js/23ec8062.7f694093.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[7590],{6417:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/kotka-tech/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
