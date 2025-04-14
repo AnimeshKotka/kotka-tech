15a66ac0d8a23157c3b87ba1e09473339a4243c0
AnimeshKotka-VaraTech <animesh.kotka@infrovate.com>
2025-04-14 21:37:30 +0530

    security and more added

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
-export default function Tag({ children, color }) {
+const titleColors = {
+  h2: '#3a731c',
+  h3: '#33A1FF', // Blue
+  h4: '#33FF77', // Green
+};
+
+export default function Tag({ children, color, title }) {
+  const computedColor = title ? titleColors[title] || color : color;
+
   return (
     <span
       style={{
-        backgroundColor: color,
+        backgroundColor: computedColor,
         borderRadius: '4px',
         color: '#fff',
         padding: '0.2rem 0.5rem',
